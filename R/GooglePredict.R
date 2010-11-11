d91676a7762741b829815b3f770bbc3942832c15
Paolo <paolo.sonego@gmail.com>
2010-11-11 16:42:29 +0100

    mixture in GooglePredict

--- R/GooglePredict.R
+++ R/GooglePredict.R
@@ -5,7 +5,7 @@ function(auth_token, mybucket, mydata, myinput) {
   curlPerform(url = paste("https://www.googleapis.com/prediction/v1.1/training/", mybucket, "%2F", mydata, "/predict", sep=""),
     customrequest = "POST",
     httpheader = c(Authorization = paste("GoogleLogin auth=", auth_token, sep = ""), 'Content-Type' = "application/json"),
-    postfields = paste("{\"data\":{ \"input\" : { \"text\" : [ \"", myinput, "\" ]}}}", sep=""),
+    postfields = paste("{\"data\":{ \"input\" : { \"mixture\" : [ \"", myinput, "\" ]}}}", sep=""),
     headerfunction = h$update,
     writefunction = b$update
     )
